81a0c5af27a48dcb5a19f9efa2ef25b7d1c0f370
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-04-03 16:44:26 +0530

    Add files via upload

--- Deletion.java
+++ Deletion.java
@@ -1,33 +1,109 @@
+class Node {
+
+    int data;
+    Node next;
+    
+    Node(int data) {
+
+        this.data = data;
+        this.next = null;
+    }
+
+}
+
 public class Main {
+    
+    static void linkedListTraversal(Node head) {
 
-    static void display(int[] arr, int n) {
+        Node ptr = head;
+        while (ptr != null) {
 
-        // Code for Traversal
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-
-        System.out.println();
-    }
-
-    static void indDeletion(int[] arr, int size, int index) {
-
-        // code for Deletion
-        for (int i = index; i < size - 1; i++) {
-
-            arr[i] = arr[i + 1];
+            System.out.println("Element : " + ptr.data);
+            ptr = ptr.next;
         }
 
     }
+    
+    // Case 1: Deleting the first element from the linked list
+    static Node deleteFirst(Node head) {
 
+        Node ptr = head;
+        head = head.next;
+        ptr = null; // releasing memory
+        return head;
+    }
+    
+    // Case 2: Deleting the element at a given index from the linked list
+    static Node deleteAtIndex(Node head, int index) {
+
+        Node p = head;
+        Node q = head.next;
+        for (int i = 0; i < index - 1; i++) {
+            p = p.next;
+            q = q.next;
+        }
+        
+        p.next = q.next;
+        q = null; // releasing memory
+        return head;
+    }
+    
+    // Case 3: Deleting the last element
+    static Node deleteAtLast(Node head) {
+
+        Node p = head;
+        Node q = head.next;
+        while (q.next != null) {
+
+            p = p.next;
+            q = q.next;
+        }
+        
+        p.next = null;
+        q = null; // releasing memory
+        return head;
+    }
+    
+    // Case 4: Deleting the element with a given value from the linked list
+    static Node deleteWithValue(Node head, int value) {
+
+        Node p = head;
+        Node q = head.next;
+        while (q.data != value && q.next != null) {
+
+            p = p.next;
+            q = q.next;
+        }
+        
+        if (q.data == value) {
+
+            p.next = q.next;
+            q = null; // releasing memory
+        }
+
+        return head;
+    }
+    
     public static void main(String[] args) {
 
-        int[] arr = {7, 8, 12, 27, 88};
-        int size = 5, element = 45, index = 0;
-        display(arr, size);
-        indDeletion(arr, size, index);
-        size -= 1;
-        display(arr, size);
+        Node head = new Node(4);
+        Node second = new Node(3);
+        Node third = new Node(8);
+        Node fourth = new Node(1);
+        
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        
+        System.out.println("Linked List Before Deletion");
+        linkedListTraversal(head);
+        
+        // head = deleteFirst(head); // For deleting first element of the linked list
+        // head = deleteAtIndex(head, 2);
+        head = deleteAtLast(head);
+        
+        System.out.println("Linked List After Deletion");
+        linkedListTraversal(head);
     }
 
 }
