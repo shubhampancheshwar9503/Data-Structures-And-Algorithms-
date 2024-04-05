aa470c3007ef337c7876f192ed4ce7490d6636fe
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-04-05 12:25:36 +0530

    Add files via upload

--- Insertion.java
+++ Insertion.java
@@ -1,40 +1,98 @@
-public class Main {
+class Node {
 
-    static void display(int[] arr, int n) {
+    int data;
+    Node next;
 
-        // Code for Traversal
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+    Node(int data) {
 
-        System.out.println();
+        this.data = data;
+        this.next = null;
     }
 
-    static int indInsertion(int[] arr, int size, int element, int capacity, int index) {
+}
 
-        // code for Insertion
-        if (size >= capacity) {
+public class Main {
 
-            return -1;
+    static void linkedListTraversal(Node head) {
+
+        Node ptr = head;
+        while (ptr != null) {
+            System.out.println("Element : " + ptr.data);
+            ptr = ptr.next;
         }
 
-        for (int i = size - 1; i >= index; i--) {
+    }
 
-            arr[i + 1] = arr[i];
+    // Case 1
+    static Node insertAtFirst(Node head, int data) {
+
+        Node ptr = new Node(data);
+        ptr.next = head;
+        return ptr;
+    }
+
+    // Case 2
+    static Node insertAtIndex(Node head, int data, int index) {
+
+        Node ptr = new Node(data);
+        Node p = head;
+        int i = 0;
+
+        while (i != index - 1) {
+
+            p = p.next;
+            i++;
         }
 
-        arr[index] = element;
-        return 1;
+        ptr.next = p.next;
+        p.next = ptr;
+        return head;
+    }
+
+    // Case 3
+    static Node insertAtEnd(Node head, int data) {
+
+        Node ptr = new Node(data);
+        Node p = head;
+
+        while (p.next != null) {
+
+            p = p.next;
+        }
+
+        p.next = ptr;
+        ptr.next = null;
+        return head;
+    }
+
+    // Case 4
+    static Node insertAfterNode(Node head, Node prevNode, int data) {
+
+        Node ptr = new Node(data);
+        ptr.next = prevNode.next;
+        prevNode.next = ptr;
+        return head;
     }
 
     public static void main(String[] args) {
 
-        int[] arr = {7, 8, 12, 27, 88};
-        int size = 5, element = 45, index = 1;
-        display(arr, size);
-        indInsertion(arr, size, element, 100, index);
-        size += 1;
-        display(arr, size);
+        Node head = new Node(7);
+        Node second = new Node(11);
+        Node third = new Node(41);
+        Node fourth = new Node(66);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        System.out.println("Linked List Before Insertion");
+        linkedListTraversal(head);
+        // head = insertAtFirst(head, 56);
+        // head = insertAtIndex(head, 56, 1);
+        // head = insertAtEnd(head, 56);
+        head = insertAfterNode(head, third, 45);
+        System.out.println("\nLinked List After Insertion");
+        linkedListTraversal(head);
     }
 
 }
